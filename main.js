ab3ec223c6a81a632f5262d04c749448d761de9e
kshitij <kshitijmurabatte@gmail.com>
2021-05-10 23:25:48 +0530

    Repo create
    Working on basic template

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const size = 5;
 
-let rows = size;
-let cols = size;
+let rows = size, cols = size;
 
 let grid = new Array(rows);
 let vis = new Array(rows);
@@ -9,24 +8,34 @@ let vis = new Array(rows);
 let openSet = new Map();
 let closedSet = new Map();
 
+let w, h;
+
 // cost to get to a node (f)
 // h -> heuristic based cost
 class cell {
 
-    constructor() {
+    constructor(i, j) {
+        this.i = i;
+        this.j = j;
+
         this.f = 0;
         this.g = 0;
         this.h = 0;
     }
 
     disp() {
-        
+        fill(255);
+        //stroke(0);
+        rect(this.i * w, this.j * h, w - 1, h - 1);
     }
 }
 
 function setup() {
 
     createCanvas(400, 400);
+
+    w = width / cols;
+    h = height / rows;
 
     // Create a 2D grid to represent the graph
     for (let i = 0; i < size; ++i) {
@@ -39,7 +48,7 @@ function setup() {
         // each grid point now has a cell object
         // and all nodes are marked unvisited
         for (let j = 0; j < cols; ++j) {
-            grid[i][j] = new cell();
+            grid[i][j] = new cell(i, j);
             vis[i][j] = 0;
         }
     }
@@ -47,8 +56,14 @@ function setup() {
 
 function draw() {
 
+        if (openSet.size > 0)
+        {
 
-
+        }
+        else
+        {
+            
+        }
 
     background(0);
 
